5153fcf779a628584e13ae842228bce9fc6b7c1e
Michael Farias <mfarias496@gmail.com>
2021-11-11 21:34:21 -0500

    Added endpoint to scrape a sport's odds

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+import scrape_data as scraper
 
+# Initialize the Flask application.
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# Configure indentation for JSON response.
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+# Add support for cross-origin requests.
+CORS(app)
+
+# Endpoint
+@app.route("/api/odds/<sport_league>")
+def get_odds_for_sport(sport_league):
+    response = scraper.get_scraped_json(sport_league)
+    return jsonify(response)
