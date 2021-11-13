cd5abfc9e062b5c3fd4129a62e706906361465dd
Michael Farias <mfarias496@gmail.com>
2021-11-13 00:05:28 -0500

    Add dynamic select options using App state

--- website/src/components/Sidebar.js
+++ website/src/components/Sidebar.js
@@ -1,20 +1,14 @@
 import React from "react";
 
-const Sidebar = () => {
+const Sidebar = ({ availableSports }) => {
 	return (
 		<div className="sidebar">
 			<h2>Search Games:</h2>
 			<select class="form-select" aria-label="Default select example">
 				<option selected>All Sports</option>
-				<option value="1">MLB</option>
-				<option value="2">NBA</option>
-				<option value="3">NHL</option>
-				<option value="4">NFL</option>
-				<option value="5">NCAA Football</option>
-				<option value="6">Boxing</option>
-				<option value="7">UFC</option>
-				<option value="8">Tennis</option>
-				<option value="9">eSports</option>
+				{availableSports.map((sport_name, idx) => (
+					<option value={idx}>{sport_name.toUpperCase()}</option>
+				))}
 			</select>
 			<select class="form-select" aria-label="Default select example">
 				<option selected>All Dates</option>
