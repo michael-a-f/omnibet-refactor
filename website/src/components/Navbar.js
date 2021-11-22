7a9c5beda135318f11c030d0914d56a9b710c65f
Michael Farias <mfarias496@gmail.com>
2021-11-21 23:21:46 -0500

    Updated Navbar and added links

--- website/src/components/Navbar.js
+++ website/src/components/Navbar.js
@@ -2,65 +2,50 @@ import React from "react";
 
 const Navbar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg">
+		<nav className="navbar navbar-expand-lg bg-primary navbar-dark py-3 fixed-top">
 			<div className="container">
-				<a className="navbar-brand" href="#">
-					Navbar
+				<a href="#" className="navbar-brand">
+					OmniBet
 				</a>
+
 				<button
 					className="navbar-toggler"
 					type="button"
 					data-bs-toggle="collapse"
-					data-bs-target="#navbarSupportedContent"
-					aria-controls="navbarSupportedContent"
-					aria-expanded="false"
-					aria-label="Toggle navigation"
+					data-bs-target="#navmenu"
 				>
 					<span className="navbar-toggler-icon"></span>
 				</button>
-				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+				<div className="collapse navbar-collapse" id="navmenu">
+					<ul className="navbar-nav ms-auto">
 						<li className="nav-item">
-							<a className="nav-link active" aria-current="page" href="#">
-								Home
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
-								Link
-							</a>
-						</li>
-						<li className="nav-item dropdown">
 							<a
-								className="nav-link dropdown-toggle"
-								href="#"
-								id="navbarDropdown"
-								role="button"
-								data-bs-toggle="dropdown"
-								aria-expanded="false"
+								target="_blank"
+								href="https://github.com/michael-a-f/omnibet-refactor"
+								className="nav-link"
 							>
-								Dropdown
+								GitHub
 							</a>
-							<ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-								<li>
-									<a className="dropdown-item" href="#">
-										Action
-									</a>
-								</li>
-								<li>
-									<a className="dropdown-item" href="#">
-										Another action
-									</a>
-								</li>
-								<li>
-									<a className="dropdown-item" href="#">
-										Something else here
-									</a>
-								</li>
-							</ul>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link disabled">Disabled</a>
+							<a
+								target="_blank"
+								href="http://127.0.0.1:5000/api/odds/nba"
+								className="nav-link"
+							>
+								API
+							</a>
+						</li>
+						<li className="nav-item">
+							<a href="#instructors" className="nav-link">
+								How it Works
+							</a>
+						</li>
+						<li className="nav-item">
+							<a href="#instructors" className="nav-link">
+								Looking Ahead
+							</a>
 						</li>
 					</ul>
 				</div>
