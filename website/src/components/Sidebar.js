63b1f10d7d40dbd72a2aa807d84feee3db552a3a
Michael Farias <mfarias496@gmail.com>
2021-11-21 23:27:18 -0500

    Changed class to className to remove React warnings

--- website/src/components/Sidebar.js
+++ website/src/components/Sidebar.js
@@ -4,23 +4,25 @@ const Sidebar = ({ availableSports }) => {
 	return (
 		<div className="sidebar">
 			<h2>Search Games:</h2>
-			<select class="form-select" aria-label="Default select example">
+			<select className="form-select" aria-label="Default select example">
 				<option selected>All Sports</option>
 				{availableSports.map((sport_name, idx) => (
-					<option value={idx}>{sport_name.toUpperCase()}</option>
+					<option key={idx} value={idx}>
+						{sport_name.toUpperCase()}
+					</option>
 				))}
 			</select>
-			<select class="form-select" aria-label="Default select example">
+			<select className="form-select" aria-label="Default select example">
 				<option selected>All Dates</option>
 				<option value="1">Today</option>
 				<option value="2">Upcoming</option>
 			</select>
-			<label for="bet-amount-slider" class="form-label">
+			<label htmlFor="bet-amount-slider" className="form-label">
 				Bet Amount
 			</label>
 			<input
 				type="range"
-				class="form-range"
+				className="form-range"
 				min="0"
 				max="5"
 				step="0.5"
