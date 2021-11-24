4ff8838c1fb702dd449f3a0e40caddfe16e74956
Michael Farias <mfarias496@gmail.com>
2021-11-23 23:53:41 -0500

    Added bet amount state variable as prop

--- website/src/components/MatchupGrid.js
+++ website/src/components/MatchupGrid.js
@@ -1,11 +1,15 @@
 import React from "react";
 import MatchupCard from "./MatchupCard";
 
-const MatchupGrid = ({ matchups }) => {
+const MatchupGrid = ({ matchups, betAmount }) => {
 	return (
 		<div className="d-flex flex-row flex-wrap justify-content-between align-items-center">
 			{matchups.map((matchup, idx) => (
-				<MatchupCard key={idx} matchup={matchup}></MatchupCard>
+				<MatchupCard
+					key={idx}
+					matchup={matchup}
+					betAmount={betAmount}
+				></MatchupCard>
 			))}
 		</div>
 	);
