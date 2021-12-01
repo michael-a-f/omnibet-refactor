aa0cb1f45387b97929f20951909e1e0cbe1734e3
Michael Farias <mfarias496@gmail.com>
2021-11-30 23:58:13 -0500

    11/30 UI update. Wording and Bootstrap styling

--- website/src/components/Boxes.js
+++ website/src/components/Boxes.js
@@ -2,57 +2,52 @@ import React from "react";
 
 const Boxes = () => {
 	return (
-		<section className="bg-dark p-5">
+		<section id="info-boxes" className="bg-dark p-4">
 			<div className="container">
 				<div className="row text-center g-4">
 					<div className="col-md">
-						<div className="card bg-dark text-light">
+						<div className="card bg-dark text-light pb-4">
 							<div className="card-body text-center">
 								<div className="h1 mb-3">
 									<i className="bi bi-laptop"></i>
 								</div>
-								<h3 className="card-title mb-3">Virtual</h3>
+								<h3 className="card-title mb-3">
+									User Focused
+								</h3>
 								<p className="card-text">
-									Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-									Iure, quas quidem possimus dolorum esse eligendi?
+									OmniBet makes it quick and easy to browse
+									and filter through all kinds of bets.
 								</p>
-								<a href="#" className="btn btn-primary">
-									Read More
-								</a>
 							</div>
 						</div>
 					</div>
 					<div className="col-md">
-						<div className="card bg-secondary text-light">
+						<div className="card bg-dark text-light pb-4">
 							<div className="card-body text-center">
 								<div className="h1 mb-3">
 									<i className="bi bi-person-square"></i>
 								</div>
-								<h3 className="card-title mb-3">Hybrid</h3>
+								<h3 className="card-title mb-3">Math-Driven</h3>
 								<p className="card-text">
-									Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-									Iure, quas quidem possimus dolorum esse eligendi?
+									We 'rank' bets so that you can understand
+									when a bet is better than another.
 								</p>
-								<a href="#" className="btn btn-dark">
-									Read More
-								</a>
 							</div>
 						</div>
 					</div>
 					<div className="col-md">
-						<div className="card bg-dark text-light">
+						<div className="card bg-dark text-light pb-4">
 							<div className="card-body text-center">
 								<div className="h1 mb-3">
 									<i className="bi bi-people"></i>
 								</div>
-								<h3 className="card-title mb-3">In Person</h3>
+								<h3 className="card-title mb-3">
+									Whole Market
+								</h3>
 								<p className="card-text">
-									Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-									Iure, quas quidem possimus dolorum esse eligendi?
+									We scrape odds from 7 major sportsbooks so
+									that you can be sure your bet is the best.
 								</p>
-								<a href="#" className="btn btn-primary">
-									Read More
-								</a>
 							</div>
 						</div>
 					</div>
