a0f59889a2b05f0954fff4a3c9fd316e5a4d04f0
Michael Farias <mfarias496@gmail.com>
2021-12-01 00:11:02 -0500

    11/30 UI update. Team names in card header

--- website/src/components/MatchupCard.js
+++ website/src/components/MatchupCard.js
@@ -4,70 +4,74 @@ import TeamBet from "./TeamBet";
 import { useState } from "react";
 
 const MatchupCard = ({ matchup, betAmount }) => {
-	const teams = [matchup.team_1, matchup.team_2];
+  const teams = [matchup.team_1, matchup.team_2];
 
-	// Calculate the expected return from the best bet.
-	const getExpectedReturn = (team) => {
-		// None check for sake of using in calculations.
-		let moneyMultiplier = team.money_multiplier;
-		if (moneyMultiplier == null) {
-			moneyMultiplier = 0;
-		}
+  // Calculate the expected return from the best bet.
+  const getExpectedReturn = (team) => {
+    // None check for sake of using in calculations.
+    let moneyMultiplier = team.money_multiplier;
+    if (moneyMultiplier == null) {
+      moneyMultiplier = 0;
+    }
 
-		const winAmount = betAmount * moneyMultiplier - betAmount;
+    const winAmount = betAmount * moneyMultiplier - betAmount;
 
-		const expectedValue =
-			winAmount * team.win_probability - betAmount * (1 - team.win_probability);
-		const expectedReturn = 100 * ((betAmount + expectedValue) / betAmount - 1);
+    const expectedValue =
+      winAmount * team.win_probability - betAmount * (1 - team.win_probability);
+    const expectedReturn = 100 * ((betAmount + expectedValue) / betAmount - 1);
 
-		return expectedReturn;
-	};
+    return expectedReturn;
+  };
 
-	const getExpectedWinnings = (team) => {
-		// None check for sake of using in calculations.
-		let moneyMultiplier = team.money_multiplier;
-		if (moneyMultiplier == null) {
-			moneyMultiplier = 0;
-		}
+  const getExpectedWinnings = (team) => {
+    // None check for sake of using in calculations.
+    let moneyMultiplier = team.money_multiplier;
+    if (moneyMultiplier == null) {
+      moneyMultiplier = 0;
+    }
 
-		const winAmount = betAmount * moneyMultiplier - betAmount;
+    const winAmount = betAmount * moneyMultiplier - betAmount;
 
-		const expectedValue =
-			winAmount * team.win_probability - betAmount * (1 - team.win_probability);
+    const expectedValue =
+      winAmount * team.win_probability - betAmount * (1 - team.win_probability);
 
-		return expectedValue;
-	};
+    return expectedValue;
+  };
 
-	teams[0]["expected_return"] = getExpectedReturn(teams[0]);
-	teams[1]["expected_return"] = getExpectedReturn(teams[1]);
+  teams[0]["expected_return"] = getExpectedReturn(teams[0]);
+  teams[1]["expected_return"] = getExpectedReturn(teams[1]);
 
-	// State variables
-	const [selectedTeam, setSelectedTeam] = useState(matchup.team_1);
+  // State variables
+  const [selectedTeam, setSelectedTeam] = useState(matchup.team_1);
 
-	return (
-		<div className="matchup-card mb-4">
-			<p className="matchup-card-header">
-				{matchup.date} @ {matchup.time}
-			</p>
+  return (
+    <div className="matchup-card">
+      <p className="matchup-card-header m-0">
+        {matchup.sport.toUpperCase()}: {matchup.team_1.full_name} @{" "}
+        {matchup.team_2.full_name}
+      </p>
+      {/* <p className="matchup-card-header m-0">
+        {matchup.date} {matchup.time}
+      </p> */}
 
-			<div className="card-body d-flex flex-column justify-content-between">
-				{teams.map((team, idx) => (
-					<TeamBet
-						key={idx}
-						sport={matchup.sport}
-						team={team}
-						betAmount={betAmount}
-					></TeamBet>
-				))}
-			</div>
+      <div className="card-body d-flex flex-column justify-content-between">
+        {teams.map((team, idx) => (
+          <TeamBet
+            key={idx}
+            sport={matchup.sport}
+            team={team}
+            betAmount={betAmount}
+          ></TeamBet>
+        ))}
+      </div>
 
-			<BetGrade
-				expectedWinnings={getExpectedWinnings(selectedTeam)}
-				betAmount={betAmount}
-				teamName={selectedTeam.short_name}
-			></BetGrade>
-		</div>
-	);
+      <BetGrade
+        expectedWinnings={getExpectedWinnings(selectedTeam)}
+        betAmount={betAmount}
+        teamName={selectedTeam.short_name}
+      ></BetGrade>
+    </div>
+  );
 };
 
 export default MatchupCard;
