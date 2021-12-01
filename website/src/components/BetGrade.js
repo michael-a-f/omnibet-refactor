b4afc3c3cf1ef058086286b9b3349b2499c00c9e
Michael Farias <mfarias496@gmail.com>
2021-11-30 23:51:30 -0500

    11/30 UI update.  Wording and margin

--- website/src/components/BetGrade.js
+++ website/src/components/BetGrade.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 const BetGrade = ({ expectedWinnings, betAmount, teamName }) => {
-	return (
-		<div className="bet-details d-flex flex-row justify-content-between align-items-center mt-2">
-			<p className="mb-0">Exp. Winnings: ${expectedWinnings.toFixed(0)}</p>
-			<button className="btn btn-primary btn-sm">
-				Bet ${betAmount} on {teamName}
-			</button>
-		</div>
-	);
+  return (
+    <div className="bet-details d-flex flex-row justify-content-between align-items-center mt-1">
+      <p className="mb-0">Exp. Profit: ${expectedWinnings.toFixed(0)}</p>
+      <button className="btn btn-primary btn-sm">
+        Bet ${betAmount} on {teamName}
+      </button>
+    </div>
+  );
 };
 
 export default BetGrade;
