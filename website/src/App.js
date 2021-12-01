96f823ebb6b3943502b3358092fe716bbddc6bf6
Michael Farias <mfarias496@gmail.com>
2021-11-30 23:55:46 -0500

    Ran Prettier

--- website/src/App.js
+++ website/src/App.js
@@ -10,99 +10,97 @@ import Boxes from "./components/Boxes";
 import SortBar from "./components/SortBar";
 
 const App = () => {
-	// This state variable controls which sports we scrape odds for, as well
-	// as which sports are displayed in the dropdown to filter by.
-	const [availableSports, setAvailableSports] = useState([
-		"nba",
-		"nhl",
-		// "ufc",
-		// "ncaab",
-		// "ncaaf",
-		// "nfl",
-		// "boxing",
-	]);
-	const [matchups, setMatchups] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
-	const [betAmount, setbetAmount] = useState(200);
-	const [matchupFilters, setmatchupFilters] = useState({
-		sports: availableSports,
-		dates: "all",
-		sort_by: "smartest",
-	});
+  // This state variable controls which sports we scrape odds for, as well
+  // as which sports are displayed in the dropdown to filter by.
+  const [availableSports, setAvailableSports] = useState([
+    "nba",
+    "nhl",
+    // "ufc",
+    // "ncaab",
+    // "ncaaf",
+    // "nfl",
+    // "boxing",
+  ]);
+  const [matchups, setMatchups] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [betAmount, setbetAmount] = useState(200);
+  const [matchupFilters, setmatchupFilters] = useState({
+    sports: availableSports,
+    dates: "all",
+    sort_by: "smartest",
+  });
 
-	const fetchAllMatchups = async () => {
-		// Create HTTP request for each sport-- each sport has own webpage.
-		const httpRequests = [];
-		availableSports.forEach((sport) => {
-			httpRequests.push(axios(`http://127.0.0.1:5000/api/odds/${sport}`));
-		});
+  const fetchAllMatchups = async () => {
+    // Create HTTP request for each sport-- each sport has own webpage.
+    const httpRequests = [];
+    availableSports.forEach((sport) => {
+      httpRequests.push(axios(`http://127.0.0.1:5000/api/odds/${sport}`));
+    });
 
-		// Await response for all http requests.
-		const response = await Promise.all(httpRequests);
+    // Await response for all http requests.
+    const response = await Promise.all(httpRequests);
 
-		// Response is list of lists. Loop through and make 1D array of all.
-		const allMatchups = [];
-		response.forEach((sport) => {
-			sport.data.forEach((match) => {
-				allMatchups.push(match);
-			});
-		});
+    // Response is list of lists. Loop through and make 1D array of all.
+    const allMatchups = [];
+    response.forEach((sport) => {
+      sport.data.forEach((match) => {
+        allMatchups.push(match);
+      });
+    });
 
-		// 1D array of ALL matchup objects.
-		return allMatchups;
-	};
+    // 1D array of ALL matchup objects.
+    return allMatchups;
+  };
 
-	// On first render, fetch all odds, set as state, and add to session.
-	useEffect(() => {
-		fetchAllMatchups().then((resolvedMatchups) => {
-			setMatchups(resolvedMatchups);
-			sessionStorage.setItem("odds", JSON.stringify(resolvedMatchups));
-		});
-	}, []);
+  // On first render, fetch all odds, set as state, and add to session.
+  useEffect(() => {
+    fetchAllMatchups().then((resolvedMatchups) => {
+      setMatchups(resolvedMatchups);
+      sessionStorage.setItem("odds", JSON.stringify(resolvedMatchups));
+    });
+  }, []);
 
-	// On change to matchupFilters, update matchups array in state.
-	useEffect(() => {
-		// Start with all the matchups held in session.
-		var allMatchups = JSON.parse(sessionStorage.getItem("odds"));
+  // On change to matchupFilters, update matchups array in state.
+  useEffect(() => {
+    // Start with all the matchups held in session.
+    var allMatchups = JSON.parse(sessionStorage.getItem("odds"));
 
-		// Filter for matchups of selected sport with the desired date filter.
-		var displayMatchups = allMatchups.filter((matchup) => {
-			return matchupFilters.sports.includes(matchup.sport);
-		});
+    // Filter for matchups of selected sport with the desired date filter.
+    var displayMatchups = allMatchups.filter((matchup) => {
+      return matchupFilters.sports.includes(matchup.sport);
+    });
 
-		// Sort according to criteria.
+    // Sort according to criteria.
 
-		// Update state variable
-		setMatchups(displayMatchups);
-	}, [matchupFilters]);
+    // Update state variable
+    setMatchups(displayMatchups);
+  }, [matchupFilters]);
 
-	return (
-		<div className="App">
-			<Navbar></Navbar>
-			<Showcase></Showcase>
-			<Boxes></Boxes>
-			<section id="main-content">
-				<div className="container py-5">
-					<div className="row">
-						<div className="col-xl-3 pb-4">
-							<Sidebar
-								availableSports={availableSports}
-							></Sidebar>
-						</div>
-						<div className="col-xl-9">
-							<Email></Email>
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Showcase></Showcase>
+      <Boxes></Boxes>
+      <section id="main-content">
+        <div className="container py-5">
+          <div className="row">
+            <div className="col-xl-3 pb-4">
+              <Sidebar availableSports={availableSports}></Sidebar>
+            </div>
+            <div className="col-xl-9">
+              <Email></Email>
 
-							<SortBar matchupCount={matchups.length}></SortBar>
-							<MatchupGrid
-								matchups={matchups}
-								betAmount={betAmount}
-							></MatchupGrid>
-						</div>
-					</div>
-				</div>
-			</section>
-		</div>
-	);
+              <SortBar matchupCount={matchups.length}></SortBar>
+              <MatchupGrid
+                matchups={matchups}
+                betAmount={betAmount}
+              ></MatchupGrid>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default App;
