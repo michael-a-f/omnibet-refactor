c5019384e31f1758427a87fc6e5bc1da0b44f14f
Michael Farias <mfarias496@gmail.com>
2021-12-01 00:12:31 -0500

    11/30 UI update. Change from Bootstrap grid to CSS grid

--- website/src/components/MatchupGrid.js
+++ website/src/components/MatchupGrid.js
@@ -2,17 +2,17 @@ import React from "react";
 import MatchupCard from "./MatchupCard";
 
 const MatchupGrid = ({ matchups, betAmount }) => {
-	return (
-		<div className="d-flex flex-row flex-wrap justify-content-between align-items-center">
-			{matchups.map((matchup, idx) => (
-				<MatchupCard
-					key={idx}
-					matchup={matchup}
-					betAmount={betAmount}
-				></MatchupCard>
-			))}
-		</div>
-	);
+  return (
+    <div className="matchup-grid">
+      {matchups.map((matchup, idx) => (
+        <MatchupCard
+          key={idx}
+          matchup={matchup}
+          betAmount={betAmount}
+        ></MatchupCard>
+      ))}
+    </div>
+  );
 };
 
 export default MatchupGrid;
