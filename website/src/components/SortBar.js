31c9d1d5f6abe461cc41e24321ecc576e2a40543
Michael Farias <mfarias496@gmail.com>
2021-12-28 20:48:17 -0500

    Add onClick for sorting matchups using state variable

--- website/src/components/SortBar.js
+++ website/src/components/SortBar.js
@@ -1,20 +1,27 @@
 import React from "react";
 
-const SortBar = ({ matchupCount }) => {
-	return (
-		<div className="d-flex flex-row justify-content-between mb-4">
-			<div id="sort-by">
-				<select className="form-select" aria-label="Default select example">
-					<option selected>Smartest bets first</option>
-					<option value="1">Biggest underdogs first</option>
-					<option value="2">Biggest favorites first</option>
-				</select>
-			</div>
-			<div id="matchup-count">
-				<p>Showing 1-20 out of {matchupCount} games</p>
-			</div>
-		</div>
-	);
+const SortBar = ({ matchupCount, onFilterSports }) => {
+  return (
+    <div className="d-flex flex-row justify-content-between mb-4">
+      <div id="sort-by">
+        <select
+          id="sort-by"
+          className="form-select"
+          aria-label="Default select example"
+          onChange={(e) => onFilterSports(e.target)}
+        >
+          <option selected value="smartest">
+            Smartest bets first
+          </option>
+          <option value="underdogs">Biggest underdogs first</option>
+          <option value="favorites">Biggest favorites first</option>
+        </select>
+      </div>
+      <div id="matchup-count">
+        <p>Showing 1-20 out of {matchupCount} games</p>
+      </div>
+    </div>
+  );
 };
 
 export default SortBar;
