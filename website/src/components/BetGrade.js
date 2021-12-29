8bfe1b3ff65032628fd8048514f2ef860c049a35
Michael Farias <mfarias496@gmail.com>
2021-12-28 20:37:32 -0500

    Matchup cards get dynamic grade based on ROI

--- website/src/components/BetGrade.js
+++ website/src/components/BetGrade.js
@@ -1,11 +1,26 @@
 import React from "react";
 
-const BetGrade = ({ expectedWinnings, betAmount, teamName }) => {
+const BetGrade = ({ betAmount, team, expectedROI }) => {
+  let grade;
+  if (expectedROI < 4) {
+    grade = "Poor";
+  } else if (expectedROI >= 4 && expectedROI < 7) {
+    grade = "OK";
+  } else if (expectedROI >= 7 && expectedROI < 16) {
+    grade = "Good";
+  } else if (expectedROI >= 16 && expectedROI < 25) {
+    grade = "Great";
+  } else if (expectedROI >= 25) {
+    grade = "Excellent";
+  }
+  grade = `${grade} bet`;
+
   return (
     <div className="bet-details d-flex flex-row justify-content-between align-items-center mt-1">
-      <p className="mb-0">Exp. Profit: ${expectedWinnings.toFixed(0)}</p>
+      <p className="mb-0">Exp. ROI: {expectedROI.toFixed(2)}%</p>
+      <p className="mb-0">{grade}</p>
       <button className="btn btn-primary btn-sm">
-        Bet ${betAmount} on {teamName}
+        Bet ${betAmount} on {team.short_name}
       </button>
     </div>
   );
