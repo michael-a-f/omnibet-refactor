55cc219f314ccc6765ee402f922dd28776d2c2d5
Michael Farias <mfarias496@gmail.com>
2021-12-28 20:46:55 -0500

    Added function to filter based on click of HTML select option

--- website/src/components/Sidebar.js
+++ website/src/components/Sidebar.js
@@ -1,21 +1,35 @@
 import React from "react";
 
-const Sidebar = ({ availableSports }) => {
+const Sidebar = ({ availableSports, onFilterMatchups }) => {
   return (
     <div className="sidebar">
       <h2>Search Games:</h2>
-      <select className="form-select" aria-label="Default select example">
-        <option selected>All Sports</option>
+      <select
+        id="sports-filter"
+        className="form-select"
+        aria-label="Default select example"
+        onChange={(e) => onFilterMatchups(e.target)}
+      >
+        <option selected value="all">
+          All Sports
+        </option>
         {availableSports.map((sport_name, idx) => (
-          <option key={idx} value={idx}>
+          <option key={idx} value={sport_name}>
             {sport_name.toUpperCase()}
           </option>
         ))}
       </select>
-      <select className="form-select" aria-label="Default select example">
-        <option selected>All Dates</option>
-        <option value="1">Today</option>
-        <option value="2">Upcoming</option>
+      <select
+        id="dates-filter"
+        className="form-select"
+        aria-label="Default select example"
+        onChange={(e) => onFilterMatchups(e.target)}
+      >
+        <option selected value="all">
+          All Dates
+        </option>
+        <option value="today">Today</option>
+        <option value="future">Upcoming</option>
       </select>
       <label htmlFor="bet-amount-slider" className="form-label">
         Bet Amount
