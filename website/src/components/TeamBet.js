3e76eae212ddd6cffe84785c4947514e9961f0e2
Michael Farias <mfarias496@gmail.com>
2021-12-30 16:47:19 -0500

    Add onClick to select a bet for a matchup

--- website/src/components/TeamBet.js
+++ website/src/components/TeamBet.js
@@ -1,45 +1,51 @@
 import React from "react";
 
 const TeamBet = ({
-  sport,
-  team,
-  selectedTeam,
-  expectedProfit,
-  expectedROI,
+	sport,
+	team,
+	selectedTeam,
+	profitOnWin,
+	expectedProfit,
+	toggleSelectedTeam,
 }) => {
-  // Modify logo filepath for sports without team-specific logos.
-  if (["boxing", "ufc", "ncaaf", "ncaab"].includes(sport)) {
-    team.logo = `../../../img/${sport}.png`;
-  }
+	// Modify logo filepath for sports without team-specific logos.
+	if (["boxing", "ufc", "ncaaf", "ncaab"].includes(sport)) {
+		team.logo = `../../../img/${sport}.png`;
+	}
 
-  const winProbability = (100 * team.win_probability).toFixed(0);
+	const winProbability = (100 * team.win_probability).toFixed(0);
 
-  return (
-    <div
-      className={
-        team === selectedTeam
-          ? "selected bet row align-items-center"
-          : "bet row align-items-center"
-      }
-    >
-      <div className="col text-center">
-        <img
-          className="img-fluid"
-          src={team.logo}
-          alt={team.full_name}
-          style={{ width: "4.5rem" }}
-        />
-      </div>
-      <div className="col text-center">
-        <p className="m-0">{winProbability}%</p>
-        <p className="m-0">Chance</p>
-      </div>
-      <div className="col text-center">
-        <p className="m-0">${expectedProfit.toFixed(0)}</p>
-        <p className="m-0">To Win</p>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className={
+				team === selectedTeam
+					? "selected bet row align-items-center"
+					: "bet row align-items-center"
+			}
+			onClick={() => toggleSelectedTeam(team)}
+		>
+			<div className="col text-center">
+				<img
+					className="img-fluid"
+					src={team.logo}
+					alt={team.full_name}
+					style={{ width: "4.5rem" }}
+				/>
+			</div>
+			<div className="col text-center">
+				<p className="m-0">{winProbability}%</p>
+				<p className="m-0">Chance</p>
+			</div>
+			<div className="col text-center">
+				<p className="m-0">${profitOnWin.toFixed(0)}</p>
+				<p className="m-0">To Win</p>
+			</div>
+			{/* <div className="col text-center">
+				<p className="m-0">${expectedProfit.toFixed(2)}</p>
+				<p className="m-0">Exp. Profit</p>
+			</div> */}
+		</div>
+	);
 };
 
 export default TeamBet;
