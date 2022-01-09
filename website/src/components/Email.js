a0688888df26037543c57f14bcdd1c08d3ca7696
Michael Farias <mfarias496@gmail.com>
2022-01-08 21:00:48 -0500

    Email.js component is own section now

--- website/src/components/Email.js
+++ website/src/components/Email.js
@@ -1,25 +1,25 @@
 import React from "react";
 
 const Email = () => {
-  return (
-    // <section className="email text-dark p-5 mb-4">
-    // 	<div className="container">
-    <div
-      id="email"
-      className="email p-5 mb-4 d-md-flex justify-content-evenly align-items-center"
-    >
-      <p className="mb-3 mb-md-0">Email me the best bets every day.</p>
-
-      <div className="input-group">
-        <input type="text" className="form-control" placeholder="Enter Email" />
-        <button className="btn btn-dark btn-lg" type="button">
-          Submit
-        </button>
-      </div>
-    </div>
-    // 	</div>
-    // </section>
-  );
+	return (
+		<section id="email" className="bg-dark p-5 mb-4">
+			<div className="container">
+				<div className="d-flex flex-row justify-content-evenly align-items-center">
+					<p className="mb-3 mb-md-0">Email me the best bets every day.</p>
+					<div className="input-group">
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Enter Email"
+						/>
+						<button className="btn btn-dark btn-lg" type="button">
+							Submit
+						</button>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Email;
