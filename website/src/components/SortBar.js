7def8e58978bf60cac33cb2d6f01581053775ecd
Michael Farias <mfarias496@gmail.com>
2022-01-08 21:19:53 -0500

    Changed options for Sort By

--- website/src/components/SortBar.js
+++ website/src/components/SortBar.js
@@ -1,27 +1,27 @@
 import React from "react";
 
-const SortBar = ({ matchupCount, onFilterSports }) => {
-  return (
-    <div className="d-flex flex-row justify-content-between mb-4">
-      <div id="sort-by">
-        <select
-          id="sort-by"
-          className="form-select"
-          aria-label="Default select example"
-          onChange={(e) => onFilterSports(e.target)}
-        >
-          <option selected value="smartest">
-            Smartest bets first
-          </option>
-          <option value="underdogs">Biggest underdogs first</option>
-          <option value="favorites">Biggest favorites first</option>
-        </select>
-      </div>
-      <div id="matchup-count">
-        <p>Showing 1-20 out of {matchupCount} games</p>
-      </div>
-    </div>
-  );
+const SortBar = ({ matchupCount, onFilterMatchups }) => {
+	return (
+		<div className="d-flex flex-row justify-content-between mb-4">
+			<div>
+				<select
+					id="sort-by"
+					className="form-select"
+					aria-label="Default select example"
+					onChange={(e) => onFilterMatchups(e.target)}
+				>
+					<option selected value="smartest">
+						Smartest bets first
+					</option>
+					<option value="soonest">Soonest games first</option>
+					<option value="farthest">Soonest games last</option>
+				</select>
+			</div>
+			<div id="matchup-count">
+				<p>Showing 1-20 out of {matchupCount} games</p>
+			</div>
+		</div>
+	);
 };
 
 export default SortBar;
