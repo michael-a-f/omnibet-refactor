1944816830623500be2c95ee86a9c9f13d5f8ba6
Michael Farias <mfarias496@gmail.com>
2022-01-08 20:47:27 -0500

    Added exception handling to use placeholder data on failed request

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+from requests.exceptions import HTTPError, ProxyError, Timeout
+from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
 import scrape_data as scraper
 import requests
@@ -15,28 +16,56 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 # Add support for cross-origin requests.
 CORS(app)
 
-
-@app.errorhandler(500)
-@app.errorhandler(Exception)
-def unhandled_exception_handler(error):
-    """Unhandled exception handler."""
-    return jsonify({
-        'error': 'An unexpected internal server error occurred. Please try again.'
-    }), 500
-
-
+# Error handling
+@app.errorhandler(HTTPError)
 @app.errorhandler(404)
 @app.errorhandler(405)
 def route_not_found_handler(error):
-    """Route not found handler."""
+    """Handle errors due to invalid routes.
+
+    Handles errors due to if user tries to navigate to anything besides
+    /api/odds/ or enters an invalid sport i.e. /api/sports/foosball.
+    """
     return jsonify({
         'error': 'Route not found: {0} {1}'.format(request.method, request.path)
     }), 404
 
 
+@app.errorhandler(ProxyError)
+def proxy_exception_handler(error):
+    """Handle errors due to sending HTTP request through Proxy IP
+    
+    Because sports gambling legislation differs across states, oddsshark.com 
+    might restrict content based on IP address location. To get around this, we
+    send the HTTP request through Proxy IP addresses found on sites like 
+    free-proxy-list.net or hidemy.name. These services are not very reliable,
+    so errors are quite common.
+
+    In the event of an error with the Proxy being used, our solution is to
+    simply return the data from the most recent, successful response for the 
+    given sport. The sport causing the error is passed into the description 
+    parameter (second positional argument) in the abort() method.
+    """
+    sport_causing_err = error.description
+    placeholder_response = bc.get_fake_data(sport_causing_err)
+    return jsonify(placeholder_response), 500
+
+
+@app.errorhandler(Exception)
+def unhandled_exception_handler(error):
+    """Handle errors unhandled elsewhere.
+    
+    Just to be safe and to preserve site functionality, any unhandled
+    exceptions will return the most recent, successful request
+    """
+    sport_causing_err = error.description
+    placeholder_response = bc.get_fake_data(sport_causing_err)
+    return jsonify(placeholder_response), 500
+
+
 # Endpoint
-@app.route("/api/odds/<sport_league>", methods=['GET'])
-def get_odds_for_sport(sport_league):
+@app.route("/api/odds/<sport>", methods=['GET'])
+def get_odds_for_sport(sport):
     """Endpoint to scrape oddsshark.com and return sports gambling info in JSON.
 
     The sportsbooks offering odds sit in their own container.
@@ -47,45 +76,55 @@ def get_odds_for_sport(sport_league):
     'weaves' all 3 pieces of information together to get the final output.
     
     Args:
-        sport_league (string): The name of the sports league to scrape info for.
+        sport (string): The name of the sport to scrape info for.
     
     Returns:
         List: JSON object holding information for each matchup.
     """
     selectors = scraper.SELECTORS
-   
-    # Request URL and create BS4 object. Proxy IP address used to bypass
-    # content restriction due to state legislation. Proxy address found 
-    # using https://hidemy.name/en/proxy-list/
-    url = f'https://www.oddsshark.com/{sport_league}/odds'
-    proxies = {
-        "http": "http://51.81.155.78:3128", # Hillsboro, Oregon
-        "https": "http://51.81.155.78:3128", # Hillsboro, Oregon
-    }
-    http_response = requests.get(url, proxies=proxies)
     
-    try:
-        http_response.raise_for_status()
-        page_html = BeautifulSoup(http_response.content, 'html.parser')
-    except:
-        return jsonify(bc.get_fake_data(sport_league))
+    # Catch invalid sport argument before even sending request.
+    if sport not in scraper.SPORTS:
+        abort(404, sport)
 
-    # The sportsbook names are held in the 'alt' attribute of their images.
-    sportsbook_images = page_html.select(selectors["sportsbooks"])
-    sportsbooks = [image['alt'].lower().strip() for image in sportsbook_images]
+    # Send request using Proxy to bypass content restriction due to IP location. 
+    url = f'https://www.oddsshark.com/{sport}/odds'
+    proxies = {
+        "http": "http://51.81.155.78:3128", # Florida
+        "https": "http://51.81.155.78:3128", # Florida
+    }
     
-    # Find all containers holding matchup info and create matchup dict for each one.
-    matchup_containers = page_html.find_all('div', class_= selectors["matchups"])
-    matchups = scraper.scrape_matchups(sport_league, matchup_containers, selectors)
+    # Catch various exceptions and handle using functions above. Otherwise,
+    # instantiate BS4 object to continue scraping.
+    try:
+        http_response = requests.get(url, proxies=proxies)
+        http_response.raise_for_status()  
+    except (requests.exceptions.ConnectionError, requests.exceptions.ProxyError, 
+    requests.exceptions.Timeout) as err_proxy:
+        abort(500, sport) # invoke proxy_error_handler
+    except (requests.exceptions.HTTPError) as err_http:
+        abort(404, sport) # invoke route_not_found_handler
+    except:
+        abort(500, sport) # invoke unhandled_exception_handler
+    else:
+        page_bs4 = BeautifulSoup(http_response.content, 'html.parser')
+    
+    # Scrape sportsbook names
+    sportsbook_names = scraper.scrape_sportsbook_names(page_bs4, selectors)
+    
+    # Scrape the left-hand containers holding matchup info only.
+    matchup_containers = page_bs4.find_all('div', class_= selectors["matchups"])
+    matchups = scraper.scrape_matchups(sport, matchup_containers, selectors)
 
     # Find all the game odds containers and create odds dict for each one.
-    odds_containers = page_html.select(selectors["odds"])
-    odds = [scraper.scrape_odds(odds_container, sportsbooks, selectors) for odds_container in odds_containers]
+    odds_containers = page_bs4.select(selectors["odds"])
+    odds = []
+    for container in odds_containers:
+        odds.append(scraper.scrape_odds(container, sportsbook_names, selectors))
     
-    # Check that every matchup has a corresponding odds dict.
+    # Scraping correct if every matchup has a corresponding odds dict.
     is_scraped_correctly = len(matchups) == len(odds)
 
-    # Add info from odds dict to the matchup dict to create desired output.
     if is_scraped_correctly:
         try:
             current_matchup = 0
@@ -99,11 +138,12 @@ def get_odds_for_sport(sport_league):
                     max_profit_odd = bc.optimal_odd_to_bet(team_odds)
                     matchup[team]["money_multiplier"] = bc.money_multiplier(max_profit_odd)
                     matchup[team]["win_probability"] = bc.win_probability_from_odds(team_odds)
-                
-                # Maintain correct matchup dict to odds dict pairing.
+            
                 current_matchup += 1
             return jsonify(matchups)
         except:
-            return jsonify(bc.get_fake_data(sport_league))
+            # Error in "zipping" matchups and odds.
+            abort(500, sport)
     else:
-        return jsonify(bc.get_fake_data(sport_league))
+        # Scraping of odds was done incorrectly.
+        abort(500, sport)
