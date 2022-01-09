94b27fafdd4e6cf427dc5eb7e7ffd164eed7a46a
Michael Farias <mfarias496@gmail.com>
2022-01-08 21:15:28 -0500

    Added slider to sidebar to update betAmount state var

--- website/src/components/Sidebar.js
+++ website/src/components/Sidebar.js
@@ -1,49 +1,61 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
-const Sidebar = ({ availableSports, onFilterMatchups }) => {
-  return (
-    <div className="sidebar">
-      <h2>Search Games:</h2>
-      <select
-        id="sports-filter"
-        className="form-select"
-        aria-label="Default select example"
-        onChange={(e) => onFilterMatchups(e.target)}
-      >
-        <option selected value="all">
-          All Sports
-        </option>
-        {availableSports.map((sport_name, idx) => (
-          <option key={idx} value={sport_name}>
-            {sport_name.toUpperCase()}
-          </option>
-        ))}
-      </select>
-      <select
-        id="dates-filter"
-        className="form-select"
-        aria-label="Default select example"
-        onChange={(e) => onFilterMatchups(e.target)}
-      >
-        <option selected value="all">
-          All Dates
-        </option>
-        <option value="today">Today</option>
-        <option value="future">Upcoming</option>
-      </select>
-      <label htmlFor="bet-amount-slider" className="form-label">
-        Bet Amount
-      </label>
-      <input
-        type="range"
-        className="form-range"
-        min="0"
-        max="5"
-        step="0.5"
-        id="bet-amount-slider"
-      ></input>
-    </div>
-  );
+const Sidebar = ({
+	availableSports,
+	onFilterMatchups,
+	betAmount,
+	setBetAmount,
+}) => {
+	const [sliderValue, setSliderValue] = useState(25);
+	return (
+		<div className="sidebar">
+			<p>Filter by Sport</p>
+			<select
+				id="sports-filter"
+				className="form-select mb-4"
+				aria-label="Default select example"
+				onChange={(e) => onFilterMatchups(e.target)}
+			>
+				<option key={0} value="all">
+					All Sports
+				</option>
+				{availableSports.map((sport_name, idx) => (
+					<option key={idx + 1} value={sport_name}>
+						{sport_name.toUpperCase()}
+					</option>
+				))}
+			</select>
+			<p>Filter by Date</p>
+			<select
+				id="dates-filter"
+				className="form-select"
+				aria-label="Default select example"
+				onChange={(e) => onFilterMatchups(e.target)}
+			>
+				<option selected value="all">
+					All Dates
+				</option>
+				<option value="today">Today Only</option>
+			</select>
+
+			<div className="d-flex flex-row justify-content-between align-items-center mt-5">
+				<p>Bet Amount</p>
+				<p>${betAmount}</p>
+			</div>
+
+			<input
+				type="range"
+				className="form-range"
+				min="5"
+				max="500"
+				step="5"
+				defaultValue={betAmount}
+				onChange={(e) => setBetAmount(parseInt(e.target.value))}
+				id="bet-amount-slider"
+			></input>
+		</div>
+	);
 };
 
 export default Sidebar;
