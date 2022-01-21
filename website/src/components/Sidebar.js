580c1fcbd392d2d98e5f26c0a45648fafb935a97
Michael Farias <mfarias496@gmail.com>
2022-01-20 22:16:21 -0500

    Change to onChange of Selects for sports and dates

--- website/src/components/Sidebar.js
+++ website/src/components/Sidebar.js
@@ -1,52 +1,23 @@
 import React from "react";
-import { useState, useEffect } from "react";
 
 const Sidebar = ({
 	availableSports,
-	onFilterMatchups,
+	onFilterSportsToShow,
+	datesToShow,
+	onFilterDatesToShow,
 	betAmount,
 	setBetAmount,
 }) => {
-	const [sliderValue, setSliderValue] = useState(25);
 	return (
 		<div className="sidebar">
-			<p>Filter by Sport</p>
-			<select
-				id="sports-filter"
-				className="form-select mb-4"
-				aria-label="Default select example"
-				onChange={(e) => onFilterMatchups(e.target)}
-			>
-				<option key={0} value="all">
-					All Sports
-				</option>
-				{availableSports.map((sport_name, idx) => (
-					<option key={idx + 1} value={sport_name}>
-						{sport_name.toUpperCase()}
-					</option>
-				))}
-			</select>
-			<p>Filter by Date</p>
-			<select
-				id="dates-filter"
-				className="form-select"
-				aria-label="Default select example"
-				onChange={(e) => onFilterMatchups(e.target)}
-			>
-				<option selected value="all">
-					All Dates
-				</option>
-				<option value="today">Today Only</option>
-			</select>
-
-			<div className="d-flex flex-row justify-content-between align-items-center mt-5">
+			<div className="d-flex flex-row justify-content-between align-items-center mb-2">
 				<p>Bet Amount</p>
 				<p>${betAmount}</p>
 			</div>
 
 			<input
 				type="range"
-				className="form-range"
+				className="form-range mb-3"
 				min="5"
 				max="500"
 				step="5"
@@ -54,6 +25,40 @@ const Sidebar = ({
 				onChange={(e) => setBetAmount(parseInt(e.target.value))}
 				id="bet-amount-slider"
 			></input>
+			<hr />
+			<p>Filter by Sport</p>
+			<select
+				id="sports-filter"
+				className="form-select mb-4"
+				aria-label="Default select example"
+				onChange={(e) => onFilterSportsToShow(e.target.value)}
+			>
+				<option key={0} value="all">
+					All Sports
+				</option>
+				{availableSports.map((sport, idx) => (
+					<option key={idx + 1} value={sport}>
+						{sport.toUpperCase()}
+					</option>
+				))}
+			</select>
+			<p>Filter by Date</p>
+			<select
+				id="dates-filter"
+				className="form-select mb-4"
+				aria-label="Default select example"
+				onChange={(e) => onFilterDatesToShow(e.target.value)}
+			>
+				<option selected value="all">
+					All Dates
+				</option>
+				{["Today", "Future"].map((date, idx) => (
+					<option key={idx + 1} value={date}>
+						{date}
+					</option>
+				))}
+			</select>
+			<hr />
 		</div>
 	);
 };
