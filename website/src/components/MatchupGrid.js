a4e1a71bf550fb7fa99bb3c52028df3b17ceb3a0
Michael Farias <mfarias496@gmail.com>
2022-01-20 22:11:10 -0500

    Added pagination functionality to UI

--- website/src/components/MatchupGrid.js
+++ website/src/components/MatchupGrid.js
@@ -1,7 +1,13 @@
 import React from "react";
 import MatchupCard from "./MatchupCard";
 
-const MatchupGrid = ({ matchups, betAmount, matchupFilters }) => {
+const MatchupGrid = ({
+	filteredMatchups,
+	indexOfFirstMatchup,
+	indexOfLastMatchup,
+	sortMatchupsBy,
+	betAmount,
+}) => {
 	// Get the profit ($ amount) of a team's best bet if that bet wins.
 	const getProfitOnWin = (betAmount, team) => {
 		return betAmount * (team.money_multiplier - 1);
@@ -16,111 +22,59 @@ const MatchupGrid = ({ matchups, betAmount, matchupFilters }) => {
 		return expectedProfit;
 	};
 
-	// // Get the expected ROI of a team's best bet.
-	// const getExpectedROI = (betAmount, team) => {
-	// 	const expectedReturn = getExpectedProfit(betAmount, team);
-	// 	return (betAmount + expectedReturn) / betAmount - 1;
-
-	// 	// let moneyMultiplier = team.money_multiplier;
-	// 	// if (!moneyMultiplier) {
-	// 	// 	moneyMultiplier = 0;
-	// 	// } else {
-	// 	// 	moneyMultiplier = moneyMultiplier;
-	// 	// }
-	// 	// DEBUGGING
-	// 	// const teamName = team.full_name;
-	// 	// const prizeOnWin = team.money_multiplier * betAmount;
-	// 	// const profitOnWin = getProfitOnWin(team);
-	// 	// let winProbability = team.win_probability;
-	// 	// if (!winProbability) {
-	// 	// 	winProbability = 0;
-	// 	// } else {
-	// 	// 	winProbability = winProbability;
-	// 	// }
-	// 	// const profitOnLoss = -betAmount;
-	// 	// const expectedProfitCoded = getExpectedProfit(team);
-	// 	// const expectedProfitManual =
-	// 	// 	profitOnWin * winProbability + profitOnLoss * (1 - winProbability);
-
-	// 	// console.log(`Team: ${teamName}`);
-	// 	// console.log(`Money Multiplier: ${moneyMultiplier}`);
-	// 	// console.log(`Prize On Win: $${prizeOnWin}`);
-	// 	// console.log(`Profit On Win: $${profitOnWin}`);
-	// 	// console.log(`Win Probability: ${winProbability}`);
-	// 	// console.log(`Profit on Loss: $${-betAmount}`);
-	// 	// console.log(`Expected Profit Code: $${expectedProfitCoded}`);
-	// 	// console.log(`Expected Profit Manual: $${expectedProfitManual}`);
-	// 	// console.log("");
+	// Return the team object for the better team to bet on in a matchup.
+	// const getBetterTeamToBet = (betAmount, matchup) => {
+	// 	if (
+	// 		getExpectedProfit(betAmount, matchup.team_1) >=
+	// 		getExpectedProfit(betAmount, matchup.team_2)
+	// 	) {
+	// 		return matchup.team_1;
+	// 	} else {
+	// 		return matchup.team_2;
+	// 	}
 	// };
 
-	const isToday = (someDate) => {
-		const today = new Date();
-		return (
-			someDate.getDate() == today.getDate() &&
-			someDate.getMonth() == today.getMonth() &&
-			someDate.getFullYear() == today.getFullYear()
-		);
-	};
-
-	const filterByDate = (matchup) => {
-		if (matchupFilters.dates.includes("all")) {
-			return true;
-		} else {
-			const isGameToday = isToday(new Date(matchup.datetime));
-			return isGameToday;
+	// Smarter of two matchups is the one with the best possible bet.
+	// Soonest and farthest are date-driven.
+	const sortedAndFilteredMatchups = filteredMatchups.sort(
+		(matchupA, matchupB) => {
+			if (sortMatchupsBy === "smartest") {
+				return (
+					Math.max(
+						getExpectedProfit(betAmount, matchupB.team_1),
+						getExpectedProfit(betAmount, matchupB.team_2)
+					) -
+					Math.max(
+						getExpectedProfit(betAmount, matchupA.team_1),
+						getExpectedProfit(betAmount, matchupA.team_2)
+					)
+				);
+			} else if (sortMatchupsBy === "soonest") {
+				return new Date(matchupA.datetime) - new Date(matchupB.datetime);
+			} else if (sortMatchupsBy === "farthest") {
+				return new Date(matchupB.datetime) - new Date(matchupA.datetime);
+			}
 		}
-	};
+	);
 
-	// Return the team object for the better team to bet on in a matchup.
-	const getBetterTeamToBet = (matchup) => {
-		if (
-			getExpectedProfit(betAmount, matchup.team_1) >=
-			getExpectedProfit(betAmount, matchup.team_2)
-		) {
-			return matchup.team_1;
-		} else {
-			return matchup.team_2;
-		}
-	};
+	const paginatedMatchups = sortedAndFilteredMatchups.slice(
+		indexOfFirstMatchup,
+		indexOfLastMatchup
+	);
 
 	return (
 		<div className="matchup-grid">
-			{matchups
-				.filter((matchup) => {
-					return (
-						matchupFilters.sports.includes(matchup.sport) &&
-						filterByDate(matchup)
-					);
-				})
-				.sort((matchA, matchB) => {
-					if (matchupFilters.sort_by == "soonest") {
-						return new Date(matchA.datetime) - new Date(matchB.datetime);
-					} else if (matchupFilters.sort_by == "farthest") {
-						return new Date(matchB.datetime) - new Date(matchA.datetime);
-					} else if (matchupFilters.sort_by == "smartest") {
-						return (
-							Math.max(
-								getExpectedProfit(betAmount, matchB.team_1),
-								getExpectedProfit(betAmount, matchB.team_2)
-							) -
-							Math.max(
-								getExpectedProfit(betAmount, matchA.team_1),
-								getExpectedProfit(betAmount, matchA.team_2)
-							)
-						);
-					}
-				})
-				.map((matchup, idx) => (
-					<MatchupCard
-						key={idx}
-						matchup={matchup}
-						betAmount={betAmount}
-						// getProfitOnWin={getProfitOnWin}
-						// getExpectedProfit={getExpectedProfit}
-						// // getExpectedROI={getExpectedROI}
-						// getBetterTeamToBet={getBetterTeamToBet}
-					></MatchupCard>
-				))}
+			{paginatedMatchups.map((matchup, idx) => (
+				<MatchupCard
+					key={idx}
+					matchup={matchup}
+					betAmount={betAmount}
+					getProfitOnWin={getProfitOnWin}
+					getExpectedProfit={getExpectedProfit}
+					// // getExpectedROI={getExpectedROI}
+					// getBetterTeamToBet={getBetterTeamToBet}
+				></MatchupCard>
+			))}
 		</div>
 	);
 };
