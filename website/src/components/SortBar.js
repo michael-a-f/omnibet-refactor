56befe62fa4918eca0b9ff910476906700ba25a8
Michael Farias <mfarias496@gmail.com>
2022-01-20 22:17:55 -0500

    Change to sortbar props and onChange of Select

--- website/src/components/SortBar.js
+++ website/src/components/SortBar.js
@@ -1,25 +1,20 @@
 import React from "react";
 
-const SortBar = ({ matchupCount, onFilterMatchups }) => {
+const SortBar = ({ setSortMatchupsBy }) => {
 	return (
-		<div className="d-flex flex-row justify-content-between mb-4">
-			<div>
-				<select
-					id="sort-by"
-					className="form-select"
-					aria-label="Default select example"
-					onChange={(e) => onFilterMatchups(e.target)}
-				>
-					<option selected value="smartest">
-						Smartest bets first
-					</option>
-					<option value="soonest">Soonest games first</option>
-					<option value="farthest">Soonest games last</option>
-				</select>
-			</div>
-			<div id="matchup-count">
-				<p>Showing 1-20 out of {matchupCount} games</p>
-			</div>
+		<div>
+			<select
+				id="sort-by"
+				className="form-select"
+				aria-label="Default select example"
+				onChange={(e) => setSortMatchupsBy(e.target.value)}
+			>
+				<option selected value="smartest">
+					Smartest bets first
+				</option>
+				<option value="soonest">Soonest games first</option>
+				<option value="farthest">Soonest games last</option>
+			</select>
 		</div>
 	);
 };
