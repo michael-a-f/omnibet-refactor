2ade8133e5eaecac61a84b26903e9824618e3923
Michael Farias <mfarias496@gmail.com>
2022-01-20 22:13:27 -0500

    Removing functions where not needed

--- website/src/components/MatchupCard.js
+++ website/src/components/MatchupCard.js
@@ -6,68 +6,55 @@ import { useState } from "react";
 const MatchupCard = ({
 	matchup,
 	betAmount,
-	// getProfitOnWin,
-	// getExpectedProfit,
-	// getBetterTeamToBet,
+	getProfitOnWin,
+	getExpectedProfit,
 }) => {
-	const getProfitOnWin = (betAmount, team) => {
-		return betAmount * (team.money_multiplier - 1);
-	};
+	const team_1_expectedProfit = getExpectedProfit(betAmount, matchup.team_1);
+	const team_2_expectedProfit = getExpectedProfit(betAmount, matchup.team_2);
 
-	const getExpectedProfit = (betAmount, team) => {
-		const profitOnWin = getProfitOnWin(betAmount, team);
-		return (
-			profitOnWin * team.win_probability -
-			betAmount * (1 - team.win_probability)
-		);
-	};
+	let betterTeamToBet;
+	if (team_1_expectedProfit >= team_2_expectedProfit) {
+		betterTeamToBet = matchup.team_1;
+	} else {
+		betterTeamToBet = matchup.team_2;
+	}
+	// const getBetterTeamToBet = (matchup) => {
+	// 	if (team_1_expectedProfit >= team_2_expectedProfit) {
+	// 		return matchup.team_1;
+	// 	} else {
+	// 		return matchup.team_2;
+	// 	}
+	// };
 
-	const getBetterTeamToBet = (matchup) => {
-		if (
-			getExpectedProfit(betAmount, matchup.team_1) >=
-			getExpectedProfit(betAmount, matchup.team_2)
-		) {
-			return matchup.team_1;
-		} else {
-			return matchup.team_2;
-		}
-	};
 	// Default selection is the team with highest expected ROI.
-	const [selectedTeam, setSelectedTeam] = useState(() =>
-		getBetterTeamToBet(matchup)
+	const [selectedTeam, setSelectedTeam] = useState(
+		() =>
+			// getBetterTeamToBet(matchup)
+			betterTeamToBet
 	);
 
 	// const profitonWin = getProfitOnWin(betAmount, selectedTeam);
 	// const expectedProfit = getExpectedProfit(betAmount, selectedTeam);
-	const profitOnWin = betAmount * (selectedTeam.money_multiplier - 1);
-	const expectedProfit =
-		profitOnWin * selectedTeam.win_probability -
-		betAmount * (1 - selectedTeam.win_probability);
-	const expectedROI = (betAmount + expectedProfit) / betAmount - 1;
+	// const profitOnWin = betAmount * (selectedTeam.money_multiplier - 1);
+	// const expectedProfit =
+	// 	profitOnWin * selectedTeam.win_probability -
+	// 	betAmount * (1 - selectedTeam.win_probability);
+	const expectedROI =
+		(betAmount + getExpectedProfit(betAmount, selectedTeam)) / betAmount - 1;
 
-	// const expectedROI = betAmount + expectedProfit;
-	const getExpectedROI = (betAmount, team) => {
-		const expectedReturn = getExpectedProfit(betAmount, team);
-		return (betAmount + expectedReturn) / betAmount - 1;
-	};
-
-	// When matchup changes, recalculate the better team to bet on.
+	// // When matchup changes, recalculate the better team to bet on.
 	useEffect(() => {
-		setSelectedTeam(getBetterTeamToBet(matchup));
+		// setSelectedTeam(getBetterTeamToBet(matchup));
+		setSelectedTeam(betterTeamToBet);
 	}, [matchup]);
-
-	// When the selected bet changes, compute its expected ROI to use
-	// in grading the bet.
-	let selectedTeamsExpectedROI = getExpectedROI(betAmount, selectedTeam);
-	// useEffect(() => {
-	// 	selectedTeamsExpectedROI = getExpectedROI(betAmount, selectedTeam);
-	// }, [selectedTeam]);
 
 	const title = `${matchup.sport.toUpperCase()}: ${matchup.team_1.full_name} @ 
   	${matchup.team_2.full_name}`;
 
 	// Convert backend datetime to "Mon, Jan 1 12:00PM" format.
 	const datetime = new Date(matchup.datetime);
+
+	// Date
 	const months = [
 		"Jan",
 		"Feb",
@@ -83,9 +70,12 @@ const MatchupCard = ({
 		"Dec",
 	];
 	const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-	const shortMonth = months[datetime.getMonth()];
+	const month = months[datetime.getMonth()];
 	const dayOfWeek = days[datetime.getDay()];
-	const dayNumber = datetime.getDate();
+	const dayOfMonth = datetime.getDate();
+	const formatted_date = `${dayOfWeek}, ${month} ${dayOfMonth}`;
+
+	// Time
 	let hr = datetime.getHours();
 	let min = datetime.getMinutes();
 	if (min < 10) {
@@ -95,8 +85,9 @@ const MatchupCard = ({
 	if (hr > 12) {
 		hr -= 12;
 		ampm = "PM";
+	} else if (hr == 12) {
+		ampm = "PM";
 	}
-	const formatted_date = `${dayOfWeek}, ${shortMonth} ${dayNumber}`;
 	const formatted_time = `${hr}:${min}${ampm}`;
 
 	return (
