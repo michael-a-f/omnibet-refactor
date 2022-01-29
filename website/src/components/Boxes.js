c67eb8581394cd34fa82d5ce90391fee5da3d802
Michael Farias <mfarias496@gmail.com>
2022-01-29 16:01:38 -0500

    Added alt tags to sportsbook logo imgs

--- website/src/components/Boxes.js
+++ website/src/components/Boxes.js
@@ -6,38 +6,38 @@ const Boxes = () => {
 			<div className="container">
 				<div className="row g-5 text-center">
 					<div className="col">
-						<img src="../../../img/opening.png"></img>
+						<img src="../../../img/opening.png" alt="Opening"></img>
 					</div>
 					<div className="col">
-						<img src="../../../img/bovada.png"></img>
+						<img src="../../../img/bovada.png" alt="Bovada"></img>
 					</div>
 					<div className="col">
-						<img src="../../../img/caesars.png"></img>
+						<img src="../../../img/caesars.png" alt="Caesars"></img>
 					</div>
 					<div className="col">
-						<img src="../../../img/westgate.png"></img>
+						<img src="../../../img/westgate.png" alt="Westgate"></img>
 					</div>
 					<div className="col">
-						<img src="../../../img/station.png"></img>
+						<img src="../../../img/station.png" alt="Station"></img>
 					</div>
 					<div className="col">
-						<img src="../../../img/mirage.png"></img>
+						<img src="../../../img/mirage.png" alt="Mirage"></img>
 					</div>
 					<div className="col">
-						<img src="../../../img/wynn.png"></img>
+						<img src="../../../img/wynn.png" alt="Wynn"></img>
 					</div>
 					<div className="col">
-						<img src="../../../img/betway.png"></img>
+						<img src="../../../img/betway.png" alt="BetWay"></img>
 					</div>
 					{/* <div className="col">
-						<img src="../../../img/betonline.png"></img>
+						<img src="../../../img/betonline.png" alt="BetOnline"></img>
 					</div> */}
 					<div className="col">
-						<img src="../../../img/sportsbetting.png"></img>
+						<img src="../../../img/sportsbetting.png" alt="Sportsbetting"></img>
 					</div>
 
 					{/* <div className="col">
-						<img src="../../../img/mybookie.png"></img>
+						<img src="../../../img/mybookie.png" alt="MyBookie"></img>
 					</div> */}
 				</div>
 			</div>
